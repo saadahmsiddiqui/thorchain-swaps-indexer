cc655140532b0fdeb928828074e8bf719d67d0ec
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2026-03-17 05:28:18 +0500

    fix: dont update stage on success

--- src/lib/state-machine/archive-successful.ts
+++ src/lib/state-machine/archive-successful.ts
@@ -1,9 +1,6 @@
 import winston, { createLogger } from 'winston';
 import { getHavingState, updateState } from '@/lib/transactions/indexed-hashes/repository';
-import {
-    getTransactionStage as getTransactionStageFromDb,
-    updateTransactionStage,
-} from '@/lib/transactions/tx-stages/repostiory';
+import { getTransactionStage as getTransactionStageFromDb } from '@/lib/transactions/tx-stages/repostiory';
 import { get as getTransactionStageFromNode } from '@/api/thorchain/tx-stages';
 import { getClient } from '@/database';
 import { stagesChanged } from './utils';
@@ -27,33 +24,6 @@ export default async function action() {
             if (stage) {
                 const nodeStage = await getTransactionStageFromNode(item.hash);
                 if ('code' in nodeStage) return;
-
-                await updateTransactionStage(
-                    {
-                        hash: item.hash,
-                        inbound_observed_final_count: nodeStage.inbound_observed.final_count,
-                        inbound_observed_completed: nodeStage.inbound_observed.completed,
-                        inbound_confirmation_counted_remaining_seconds:
-                            nodeStage.inbound_confirmation_counted
-                                ?.remaining_confirmation_seconds ?? null,
-                        inbound_confirmation_counted_completed:
-                            nodeStage.inbound_confirmation_counted?.completed ?? null,
-                        inbound_finalised_completed: nodeStage.inbound_finalised?.completed ?? null,
-                        swap_status_pending: nodeStage.swap_status?.pending ?? null,
-                        swap_finalised_completed: nodeStage.swap_finalised?.completed ?? null,
-                        streaming_interval: nodeStage.swap_status?.streaming?.interval ?? 0,
-                        streaming_quantity: nodeStage.swap_status?.streaming?.quantity ?? 0,
-                        streaming_count: nodeStage.swap_status?.streaming?.count ?? 0,
-                        outbound_signed_scheduled_outbound_height:
-                            nodeStage.outbound_signed?.scheduled_outbound_height ?? null,
-                        outbound_signed_completed: nodeStage.outbound_signed?.completed ?? null,
-                        outbound_delay_remaining_delay_blocks:
-                            nodeStage.outbound_delay?.remaining_delay_blocks ?? null,
-                        outbound_delay_remaining_delay_seconds:
-                            nodeStage.outbound_delay?.remaining_delay_seconds ?? null,
-                    },
-                    getClient('rw'),
-                );
 
                 const changed = stagesChanged(stage, nodeStage);
                 if (changed) {
